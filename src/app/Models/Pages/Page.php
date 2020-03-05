08fa49059fb34585a987b467c119d48b1d0dcbe1
cvaize <cvaize@gmail.com>
2020-03-05 21:26:57 +0500

    (Feat) Сделал заготовку пользователя

--- src/app/Models/Pages/Page.php
+++ src/app/Models/Pages/Page.php
@@ -7,8 +7,44 @@ use BSC\Database\MigrateInterface;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Carbon;
 
 
+/**
+ * BSC\App\Models\Pages\Page
+ *
+ * @property int $id
+ * @property string $alias
+ * @property string $path
+ * @property string|null $h1
+ * @property int|null $modelable_id
+ * @property string|null $modelable_type
+ * @property string|null $meta_title
+ * @property string|null $meta_description
+ * @property string|null $meta_keywords
+ * @property \Illuminate\Support\Carbon|null $published_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page filter($frd)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereAlias($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereH1($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereMetaDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereMetaKeywords($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereMetaTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereModelable1Id($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereModelable2Id($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereModelableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereModelableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page wherePublishedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Pages\Page whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Page extends Model implements MigrateInterface
 {
 	protected $table = 'bsc_pages';
@@ -26,6 +62,39 @@ class Page extends Model implements MigrateInterface
 		'created_at',
 		'updated_at',
 	];
+
+	public function migrate(){
+		(new Migrate($this->getTable()))->createOrTable(function (Blueprint $table, Migrate $migrate){
+			// Тут только создание колонок, никаких индексов и связей
+			// Создание тут связей/индексов повлечет за собой ошибку
+			$migrate->createOrChange($table->bigIncrements('id'));
+			$migrate->createOrChange($table->string('alias'));
+			$migrate->createOrChange($table->string('path'));
+			$migrate->createOrChange($table->string('h1')->nullable());
+			$migrate->createOrChange($table->unsignedBigInteger('modelable_id')->nullable());
+			$migrate->createOrChange($table->string('modelable_type')->nullable());
+			$migrate->createOrChange($table->string('meta_title')->nullable());
+			$migrate->createOrChange($table->string('meta_description')->nullable());
+			$migrate->createOrChange($table->string('meta_keywords')->nullable());
+			$migrate->createOrChange($table->dateTime('published_at')->nullable());
+			!$migrate->hasColumn('created_at') && $table->timestamps();
+		}, function (Blueprint $table, Migrate $migrate){
+			$migrate->unique(['alias']);
+			$migrate->unique(['path']);
+			$migrate->unique(['h1']);
+			$migrate->unique(['meta_title']);
+			$migrate->index(['modelable_id']);
+			$migrate->index(['modelable_type']);
+			$migrate->index(['modelable_id', 'modelable_type']);
+		}, function (Blueprint $table, Migrate $migrate){
+			// Пример создания/удаления связи
+//			$migrate->foreign('modelable_id', function ($foreign){
+//				$foreign->references('id')->on('bsc_users')
+//					->onUpdate('cascade')->onDelete('cascade');
+//			});
+//			$migrate->dropForeign('modelable_id');
+		});
+	}
 
 	protected $casts = [
 		'published_at'=>'datetime'
@@ -75,38 +144,149 @@ class Page extends Model implements MigrateInterface
 		return $query;
 	}
 
-	public function migrate(){
-		(new Migrate($this->getTable()))->createOrTable(function (Blueprint $table, Migrate $migrate){
-			// Тут только создание колонок, никаких индексов и связей
-			// Создание тут связей/индексов повлечет за собой ошибку
-			$migrate->createOrChange($table->bigIncrements('id'));
-			$migrate->createOrChange($table->string('alias'));
-			$migrate->createOrChange($table->string('path'));
-			$migrate->createOrChange($table->string('h1')->nullable());
-			$migrate->createOrChange($table->unsignedBigInteger('modelable_id')->nullable());
-			$migrate->createOrChange($table->unsignedBigInteger('modelable1_id')->nullable());
-			$migrate->createOrChange($table->unsignedBigInteger('modelable2_id')->nullable());
-			$migrate->createOrChange($table->string('modelable_type')->nullable());
-			$migrate->createOrChange($table->string('meta_title')->nullable());
-			$migrate->createOrChange($table->string('meta_description')->nullable());
-			$migrate->createOrChange($table->string('meta_keywords')->nullable());
-			$migrate->createOrChange($table->dateTime('published_at')->nullable());
-			!$migrate->hasColumn('created_at') && $table->timestamps();
-		}, function (Blueprint $table, Migrate $migrate){
-			$migrate->unique(['alias']);
-			$migrate->unique(['path']);
-			$migrate->unique(['h1']);
-			$migrate->unique(['meta_title']);
-			$migrate->index(['modelable_id']);
-			$migrate->index(['modelable_type']);
-			$migrate->index(['modelable_id', 'modelable_type']);
-		}, function (Blueprint $table, Migrate $migrate){
-			// Пример создания/удаления связи
-//			$migrate->foreign('modelable_id', function ($foreign){
-//				$foreign->references('id')->on('bsc_users')
-//					->onUpdate('cascade')->onDelete('cascade');
-//			});
-//			$migrate->dropForeign('modelable_id');
-		});
+	/**
+	 * @return string
+	 */
+	public function getAlias(): string
+	{
+		return $this->alias;
 	}
+
+	/**
+	 * @param string $alias
+	 */
+	public function setAlias(string $alias)
+	{
+		$this->alias = $alias;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPath(): string
+	{
+		return $this->path;
+	}
+
+	/**
+	 * @param string $path
+	 */
+	public function setPath(string $path)
+	{
+		$this->path = $path;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getH1(): string
+	{
+		return $this->h1;
+	}
+
+	/**
+	 * @param string|null $h1
+	 */
+	public function setH1(string $h1)
+	{
+		$this->h1 = $h1;
+	}
+
+	/**
+	 * @return int|null
+	 */
+	public function getModelableId(): int
+	{
+		return $this->modelable_id;
+	}
+
+	/**
+	 * @param int|null $modelable_id
+	 */
+	public function setModelableId(int $modelable_id)
+	{
+		$this->modelable_id = $modelable_id;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getModelableType(): string
+	{
+		return $this->modelable_type;
+	}
+
+	/**
+	 * @param string|null $modelable_type
+	 */
+	public function setModelableType(string $modelable_type)
+	{
+		$this->modelable_type = $modelable_type;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getMetaTitle(): string
+	{
+		return $this->meta_title;
+	}
+
+	/**
+	 * @param string|null $meta_title
+	 */
+	public function setMetaTitle(string $meta_title)
+	{
+		$this->meta_title = $meta_title;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getMetaDescription(): string
+	{
+		return $this->meta_description;
+	}
+
+	/**
+	 * @param string|null $meta_description
+	 */
+	public function setMetaDescription(string $meta_description)
+	{
+		$this->meta_description = $meta_description;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getMetaKeywords(): string
+	{
+		return $this->meta_keywords;
+	}
+
+	/**
+	 * @param string|null $meta_keywords
+	 */
+	public function setMetaKeywords(string $meta_keywords)
+	{
+		$this->meta_keywords = $meta_keywords;
+	}
+
+	/**
+	 * @return \Illuminate\Support\Carbon|null
+	 */
+	public function getPublishedAt(): Carbon
+	{
+		return $this->published_at;
+	}
+
+	/**
+	 * @param \Illuminate\Support\Carbon|null $published_at
+	 */
+	public function setPublishedAt(\Illuminate\Support\Carbon $published_at = null)
+	{
+		$this->published_at = $published_at;
+	}
+
+
 }
--- src/app/Models/Users/User.php
+++ src/app/Models/Users/User.php
@@ -11,12 +11,60 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 
 
+/**
+ * BSC\App\Models\Users\User
+ *
+ * @property int $id
+ * @property string $unique_name
+ * @property string $f_name
+ * @property string $l_name
+ * @property string $m_name
+ * @property string $email
+ * @property string $phone
+ * @property int $payment_id
+ * @property int $shipping_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereFName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereLName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereMName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User wherePaymentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereShippingId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereUniqueName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\BSC\App\Models\Users\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Model implements MigrateInterface
 {
 	protected $table = 'bsc_users';
 
 	protected $fillable = [
-		'name',
+		'unique_name',
+		'f_name',
+		'l_name',
+		'm_name',
+		'email',
+		'phone',
+		'payment_id',
+		'shipping_id',
+
+//		'full_address', // лучше вынести адреса в отдельную таблицу, так как у пользователя может быть множество адресов
+//		'street',
+//		'street_nr',
+//		'home',
+//		'apartment',
+//		'zip',
+//		'city',
+//		'state',
+//		'country',
+//		'lang', // лучше на клиенте определять язык, если человек в первый раз зашел на сайт
 	];
 
 	public function migrate(){
@@ -24,10 +72,19 @@ class User extends Model implements MigrateInterface
 			// Тут только создание колонок, никаких индексов и связей
 			// Создание тут связей/индексов повлечет за собой ошибку
 			$migrate->createOrChange($table->bigIncrements('id'));
-			$migrate->createOrChange($table->string('name'));
+			$migrate->createOrChange($table->string('unique_name'));
+			$migrate->createOrChange($table->string('f_name'));
+			$migrate->createOrChange($table->string('l_name'));
+			$migrate->createOrChange($table->string('m_name'));
+			$migrate->createOrChange($table->string('email'));
+			$migrate->createOrChange($table->string('phone', 24));
+			$migrate->createOrChange($table->unsignedTinyInteger('payment_id'));
+			$migrate->createOrChange($table->unsignedTinyInteger('shipping_id'));
 			!$migrate->hasColumn('created_at') && $table->timestamps();
 		}, function (Blueprint $table, Migrate $migrate){
-			$migrate->index(['name']);
+			$migrate->unique(['unique_name']);
+			$migrate->index(['email']);
+			$migrate->index(['phone']);
 		});
 	}
 }
